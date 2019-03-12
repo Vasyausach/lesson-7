6947aff4a6800a9c630c82ef3944c72bbe4de2fd
Vasyausach <32095634+Vasyausach@users.noreply.github.com>
2019-03-12 22:52:30 +0300

    Add files via upload

--- timer/js/script.js
+++ timer/js/script.js
@@ -1,31 +1,49 @@
-window.onload = function(){
-let date = new Date(),
-    year = date.getFullYear(),
-    month = date.getMonth() +1,
-    day = date.getDay(),
-    hours = date.getHours(),
-    minutes = date.getMinutes(),
-    seconds = date.getSeconds(),
-    miliSeconds = date.getMilliseconds();
+// setInterval (function (){
+// let date = new Date(),
+//     year = date.getFullYear(),
+//     month = date.getMonth() +1,
+//     day = date.getDay(),
+//     hours = date.getHours(),
+//     minutes = date.getMinutes(),
+//     seconds = date.getSeconds(),
+//     miliSeconds = date.getMilliseconds();
 
-let mm = month;
-if (mm < 10) {mm = '0' + mm}
+// if (month < 10) {month = '0' + month;}
 
-let dd = day;
-if (dd < 10) {dd = '0' + dd}
+// if (day < 10) {day = '0' + day;}
 
-let ss = seconds;
-if (ss <10) {ss = '0' + ss}
+// if (seconds <10) {seconds = '0' + seconds;}
 
-let min = minutes;
-if (min < 10) {min = '0' + min}
+// if (minutes < 10) {minutes = '0' + minutes;}
 
-let hh = hours;
-if (hh < 10) {hh = '0' + hh}
+// if (horse < 10) {hourse = '0' + hourse;}
 
-let time = 'Date: ' + day + ':' + month + ':' + year + '  /   Time: ' + 
-hours +':' + minutes +':' + seconds + ':' + miliSeconds;
+// if (miliSeconds < 10) {miliSeconds = '00' + miliSeconds;}
+// else if (10 < miliSeconds < 100) {miliSeconds = '0' + miliSeconds;}
 
-document.getElementsByTagName('div')[0].innerHTML = time;
-window.setTimeout(arguments.callee, 1);
-};
+// document.getElementsByTagName('div')[0].textContent = 'Date: ' + day + ':' + month + ':' + year + '  /   Time: ' + 
+// hours +':' + minutes +':' + seconds + ':' + miliSeconds;
+// }, 1);
+
+setInterval(function() { //setIntervl в который засунули функцию
+    let date = new Date(), //в функции создаем локальные переменные и задаем им значения Даты обращением к переменной Date которая
+        hours = date.getHours(),//в свою очередь имеет данные о милисекундах, сколько прошло с 1970 года и являеется грубо говоря функцией
+        minutes = date.getMinutes(),//при обращении к которой с конкретным запросом, дает искомое значение в реальном времени
+        seconds = date.getSeconds();
+
+if (hours < 10){ //это условие в котором мы говорим фунции, что если число будет одинарное (от 1 до 9) мы удет перед этим
+    hours = '0' + hours;//числом подставлять ноль для этетики
+}
+
+if (minutes < 10){
+    minutes = '0' + minutes;//тут тоже самое что и выше
+}
+
+if (seconds < 10){
+    seconds = '0' + seconds;//и тут
+}
+
+document.getElementsByTagName('div')[0].textContent = 'Time: ' + hours + ':' + minutes + ':' + seconds;
+//тут мы говорим коду, что влезь в HTML, найди блок DIV[0]-первый что по порядку попадется и добавь контент(тест+часы+двоеточие+минуты+двоеточие+секунды)
+
+}, 10);//тут мы указываем с каким интервалом будет выполняться функция
